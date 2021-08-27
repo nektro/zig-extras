4d2b9a70c8347fd5ce877eb087e1b3cad54e3cae
Meghan Denny <hello@nektro.net>
2021-08-26 20:44:50 -0700

    add addSentinel

--- src/lib.zig
+++ src/lib.zig
@@ -22,3 +22,11 @@ pub fn reduceNumber(alloc: *std.mem.Allocator, input: u64, comptime unit: u64, c
 pub fn intToFloat(n: u64) f64 {
     return @intToFloat(f64, n);
 }
+
+pub fn addSentinel(alloc: *std.mem.Allocator, comptime T: type, input: []const T, comptime sentinel: T) ![:sentinel]const T {
+    var list = try std.ArrayList(T).initCapacity(alloc, input.len + 1);
+    try list.appendSlice(input);
+    try list.append(sentinel);
+    const str = list.toOwnedSlice();
+    return str[0 .. str.len - 1 :sentinel];
+}
