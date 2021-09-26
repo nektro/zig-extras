0f9f785b9b5bc9276ffe04e4c50900c1c6612d02
Meghan Denny <hello@nektro.net>
2021-09-26 02:23:00 -0700

    add doesFileExist

--- src/lib.zig
+++ src/lib.zig
@@ -77,3 +77,13 @@ pub fn doesFolderExist(dir: ?std.fs.Dir, fpath: []const u8) !bool {
     }
     return true;
 }
+
+pub fn doesFileExist(dir: ?std.fs.Dir, fpath: []const u8) !bool {
+    const file = (dir orelse std.fs.cwd()).openFile(fpath, .{}) catch |e| switch (e) {
+        error.FileNotFound => return false,
+        error.IsDir => return true,
+        else => return e,
+    };
+    defer file.close();
+    return true;
+}
