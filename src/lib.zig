a5384d1d002b3d4f3ccdcb67d7ce35797bb79335
Meghan Denny <hello@nektro.net>
2022-08-10 18:11:24 -0700

    make range public so deps dont need to add it as a direct dep too

--- src/lib.zig
+++ src/lib.zig
@@ -1,6 +1,6 @@
 const std = @import("std");
 const string = []const u8;
-const range = @import("range").range;
+pub const range = @import("range").range;
 
 pub fn fmtByteCountIEC(alloc: std.mem.Allocator, b: u64) !string {
     return try reduceNumber(alloc, b, 1024, "B", "KMGTPEZY");
