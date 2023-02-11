b303caba8d260bb4d1a3fc7a29d983a6a56bb8ce
Meghan Denny <hello@nektro.net>
2023-02-11 13:50:25 -0800

    add skipToBoundary()

--- src/lib.zig
+++ src/lib.zig
@@ -404,3 +404,11 @@ pub fn BufIndexer(comptime T: type, comptime endian: std.builtin.Endian) type {
         }
     };
 }
+
+pub fn skipToBoundary(pos: u64, boundary: u64, reader: anytype) !void {
+    // const gdiff = counter.bytes_read % 4;
+    // for (range(if (gdiff > 0) 4 - gdiff else 0)) |_| {
+    const a = pos;
+    const b = boundary;
+    try reader.skipBytes(((a + (b - 1)) & ~(b - 1)) - a, .{});
+}
