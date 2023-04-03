b47e61c4f05e895fec868252b3a23c5111b0cd10
Meghan Denny <hello@nektro.net>
2023-04-02 21:46:34 -0700

    add readBytesAlloc

--- src/lib.zig
+++ src/lib.zig
@@ -439,3 +439,12 @@ pub fn safeAdd(a: anytype, b: anytype) @TypeOf(a) {
     }
     return a - @intCast(@TypeOf(a), -b);
 }
+
+pub fn readBytesAlloc(reader: anytype, alloc: std.mem.Allocator, len: usize) ![]u8 {
+    var list = std.ArrayListUnmanaged(u8){};
+    try list.ensureTotalCapacityPrecise(alloc, len);
+    errdefer list.deinit(alloc);
+    list.appendNTimesAssumeCapacity(0, len);
+    try reader.readNoEof(list.items[0..len]);
+    return list.items;
+}
