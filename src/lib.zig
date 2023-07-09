cfeeff77833f0e180e116859b2c7370a7c38d8f0
Meghan Denny <hello@nektro.net>
2023-07-09 13:42:05 -0700

    add FieldUnion

--- src/lib.zig
+++ src/lib.zig
@@ -542,3 +542,22 @@ pub fn to_hex(array: anytype) [array.len * 2]u8 {
     std.fmt.format(fbs.writer(), "{x}", .{std.fmt.fmtSliceHexLower(&array)}) catch unreachable;
     return res;
 }
+
+pub fn FieldUnion(comptime T: type) type {
+    const infos = std.meta.fields(T);
+
+    var fields: [infos.len]std.builtin.Type.UnionField = undefined;
+    inline for (infos, 0..) |field, i| {
+        fields[i] = .{
+            .name = field.name,
+            .type = field.type,
+            .alignment = field.alignment,
+        };
+    }
+    return @Type(std.builtin.Type{ .Union = .{
+        .layout = .Auto,
+        .tag_type = std.meta.FieldEnum(T),
+        .fields = &fields,
+        .decls = &.{},
+    } });
+}
