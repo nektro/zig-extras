54abdc5bce0b243601ef4808bce1c29e1a46cdec
Meghan Denny <hello@nektro.net>
2023-08-09 20:37:22 -0700

    add ReverseFields

--- src/lib.zig
+++ src/lib.zig
@@ -665,3 +665,14 @@ pub fn OneBiggerInt(comptime T: type) type {
     info.Int.bits += 1;
     return @Type(info);
 }
+
+pub fn ReverseFields(comptime T: type) type {
+    var info = @typeInfo(T).Struct;
+    const len = info.fields.len;
+    var fields: [len]std.builtin.Type.StructField = undefined;
+    for (0..len) |i| {
+        fields[i] = info.fields[len - 1 - i];
+    }
+    info.fields = &fields;
+    return @Type(.{ .Struct = info });
+}
