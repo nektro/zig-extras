aa4f14760e80efa703f12cff0eca6f3d23103ec7
Meghan Denny <hello@nektro.net>
2024-01-23 11:16:53 -0800

    opslice: handle pointer to array

--- src/opslice.zig
+++ src/opslice.zig
@@ -2,7 +2,7 @@ const std = @import("std");
 const string = []const u8;
 const extras = @import("./lib.zig");
 
-pub fn opslice(slice: anytype, index: usize) ?std.meta.Child(@TypeOf(slice)) {
+pub fn opslice(slice: anytype, index: usize) ?std.meta.Elem(@TypeOf(slice)) {
     if (slice.len <= index) return null;
     return slice[index];
 }
@@ -10,6 +10,19 @@ pub fn opslice(slice: anytype, index: usize) ?std.meta.Child(@TypeOf(slice)) {
 test {
     const array = [_]u8{ 0, 1, 2, 3, 4, 5 };
     const slice: []const u8 = &array;
+
+    try std.testing.expect(opslice(slice, 0) == 0);
+    try std.testing.expect(opslice(slice, 1) == 1);
+    try std.testing.expect(opslice(slice, 2) == 2);
+    try std.testing.expect(opslice(slice, 3) == 3);
+    try std.testing.expect(opslice(slice, 4) == 4);
+    try std.testing.expect(opslice(slice, 5) == 5);
+    try std.testing.expect(opslice(slice, 6) == null);
+}
+
+test {
+    const array = [_]u8{ 0, 1, 2, 3, 4, 5 };
+    const slice = &array;
 
     try std.testing.expect(opslice(slice, 0) == 0);
     try std.testing.expect(opslice(slice, 1) == 1);
