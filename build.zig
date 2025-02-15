b1d5ef095c8387aa25e0fe5fa40fce2f91144d03
Meghan Denny <hello@nektro.net>
2025-02-15 12:54:20 -0800

    add disable_llvm build param

--- build.zig
+++ build.zig
@@ -3,6 +3,7 @@ const std = @import("std");
 pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const mode = b.option(std.builtin.Mode, "mode", "") orelse .Debug;
+    const disable_llvm = b.option(bool, "disable_llvm", "use the non-llvm zig codegen") orelse false;
 
     _ = b.addModule(
         "extras",
@@ -14,6 +15,8 @@ pub fn build(b: *std.Build) void {
         .target = target,
         .optimize = mode,
     });
+    tests.use_llvm = !disable_llvm;
+    tests.use_lld = !disable_llvm;
 
     const run_tests = b.addRunArtifact(tests);
     run_tests.has_side_effects = true;
