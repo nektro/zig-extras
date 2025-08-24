bfc5a70fee3b065f523ae5c4754867c497ce87ea
Meghan Denny <hello@nektro.net>
2025-08-24 02:42:35 -0700

    asciiUpper: use dupeZ

--- src/asciiUpper.zig
+++ src/asciiUpper.zig
@@ -2,8 +2,8 @@ const std = @import("std");
 const string = []const u8;
 const extras = @import("./lib.zig");
 
-pub fn asciiUpper(alloc: std.mem.Allocator, input: string) ![]u8 {
-    var buf = try alloc.dupe(u8, input);
+pub fn asciiUpper(alloc: std.mem.Allocator, input: string) ![:0]u8 {
+    var buf = try alloc.dupeZ(u8, input);
     for (0..buf.len) |i| {
         buf[i] = std.ascii.toUpper(buf[i]);
     }
