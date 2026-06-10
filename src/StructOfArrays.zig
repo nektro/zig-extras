02301520811d7796de6e4f87961a44a6f458f702
Meghan Denny <hello@nektro.net>
2026-06-10 01:43:07 -0700

    StructOfArrays: preserve field attributes

--- src/StructOfArrays.zig
+++ src/StructOfArrays.zig
@@ -8,12 +8,18 @@ pub fn StructOfArrays(len: usize, T: type) type {
     const fields = info.fields;
     var names: [fields.len][]const u8 = undefined;
     var types: [fields.len]type = undefined;
+    var attrs: [fields.len]std.builtin.Type.StructField.Attributes = undefined;
     for (fields, 0..) |item, i| {
         names[i] = item.name;
         types[i] = [len]item.type;
+        attrs[i] = .{
+            .@"comptime" = item.is_comptime,
+            .@"align" = item.alignment,
+            .default_value_ptr = item.default_value_ptr,
+        };
     }
     if (info.is_tuple) return @Tuple(&types);
-    return @Struct(.auto, null, &names, &types, &@splat(.{}));
+    return @Struct(.auto, null, &names, &types, &attrs);
 }
 
 test {
